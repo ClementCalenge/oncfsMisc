1f4c26e4a79d730db890e82812f3c2c9e64866df
Clement Calenge <clement.calenge@gmail.com>
2017-05-30 10:09:57 +0200

    Adds several utility functions

---
+++ R/divers.R
@@ -0,0 +1,8 @@
+year <- function(x) as.POSIXlt(x)$year+1900
+
+month <- function(x) as.POSIXlt(x)$mon+1
+
+mat2spdf <- function(x, id="A")
+{
+    SpatialPolygons(list(Polygons(list(Polygon(oo)), ID=id)))
+}
